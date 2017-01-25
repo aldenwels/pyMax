940f3a0ec15ab120daa04704bf8a8d06a5a0caba
aldenwels <phil.gringer@gmail.com>
2017-01-24 21:16:34 -0500

    added messages list, and modularized functionality

--- osc.py
+++ osc.py
@@ -1,10 +1,20 @@
 from pythonosc import osc_message_builder
 from pythonosc import udp_client
 
+messages = []
+client = udp_client.UDPClient("127.0.0.1", 5005)
+
+def createMessage(m,a):
+  message = osc_message_builder.OscMessageBuilder(address = m)
+  message.add_arg(a)
+  message = message.build()
+  messages.append(message)
+
+def printMessages():
+  for m in messages:
+    client.send(m)
 
 if __name__ == "__main__":
-  client = udp_client.UDPClient("127.0.0.1", 5005)
-  msg = osc_message_builder.OscMessageBuilder(address = "/hey")
-  msg.add_arg("something special")
-  msg = msg.build()
-  client.send(msg)
+  createMessage("/one","test")
+  createMessage("/two","new")
+  printMessages()
